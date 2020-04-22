cba8d41189adbf25f7c189f265d833cc517747e2
Jianhai SU <suj@email.sc.edu>
2020-04-22 18:43:13 -0400

    lift up the class_weight parameter from RF/SVM model to offer a way to handle imbalanced dataset. Also, remove some commented code

--- BoUtil.py
+++ BoUtil.py
@@ -47,18 +47,19 @@ def mapConfig(params, domain, clf_name="RF"):
     #print("[Mapped Params]:\n{}".format(dict_params))
     return dict_params
 
-def createModel(params, clf_name):
+def createModel(params, clf_name, class_weight=None):
     if clf_name == "RF":
-        model = RandomForestClassifier(**params)
+        model = RandomForestClassifier(class_weight=class_weight, **params)
     else: # "SVM"
-        model = svm.SVC(**params)
+        model = svm.SVC(class_weight=class_weight, **params)
     return model
 
-def fitCV(configs, cv=None, X_train=None, y_train=None, domain=None, clf_name="RF"):
+def fitCV(configs, cv=None, X_train=None, y_train=None, domain=None, clf_name="RF", class_weight=None):
     fs = np.zeros((configs.shape[0], 1))
     for i, params in enumerate(configs):
         dict_params = mapConfig(params, domain, clf_name=clf_name)
-        mdl = createModel(dict_params, clf_name)
+        #print(dict_params)
+        mdl = createModel(dict_params, clf_name, class_weight)
         # For minimization: negative validation accuracy averaged all folds
         fs[i] = -np.mean(cross_val_score(mdl, X_train, y_train, cv=cv))
     return fs
@@ -99,7 +100,9 @@ def evaluation(
         dataDirPrefix, sourceDataTag, labelName,
         numFGs, numFeats, test_ratio, cv,
         domain, clf_name, itersBO,
-        saveFileNamePrefix, figID, testDataTags=None, oriDF=None):
+        saveFileNamePrefix, figID, testDataTags=None, oriDF=None, class_weight=None):
+
+    numFGs = len(numFeats)
 
     if testDataTags == None:
         transferAccsAllFGs = np.zeros(numFGs)
@@ -108,7 +111,13 @@ def evaluation(
         #    raise "oriDF should not be None because testDataTags is not None"
         transferAccsAllFGs = np.zeros((numFGs, len(testDataTags)))
 
-    resultFDName = "result_"+labelName+"_"+sourceDataTag
+    if class_weight == None:
+        weight_tag = "WeightEqual"
+    elif class_weight == "balanced":
+        weight_tag = "WeightBalanced"
+    else:
+        weight_tag = "Weight{}".format(class_weight[1])
+    resultFDName = "result_"+labelName+"_"+sourceDataTag+"_"+weight_tag
     if not os.path.exists(resultFDName):
             os.makedirs(resultFDName)
 
@@ -157,7 +166,7 @@ def evaluation(
         y_train, y_test = Y[train_index], Y[test_index]
 
         opt = GPyOpt.methods.BayesianOptimization(
-                f = partial(fitCV, X_train=X_train, y_train=y_train, cv=cv, domain=domain, clf_name=clf_name),  # function to optimize
+                f = partial(fitCV, X_train=X_train, y_train=y_train, cv=cv, domain=domain, clf_name=clf_name, class_weight=class_weight),  # function to optimize
                 domain = domain,         # box-constrains of the problem
                 acquisition_type ='EI')       # EI, MPI, LCB acquisition
         opt.run_optimization(max_iter=itersBO)
@@ -213,105 +222,4 @@ def evaluation(
 
     return transferAccsAllFGs
 
-# Train and testing on one type of data.
-# Potentionally testing on different types of data.
-#def evaluation(
-#        dataDirPrefix, sourceDataTag, labelName,
-#        numFGs, numFeats, test_ratio, cv,
-#        domain, clf_name, itersBO,
-#        saveFileNamePrefix, figID, testDataTags=None):
-#
-#    if testDataTags == None:
-#        transferAccsAllFGs = np.zeros(numFGs)
-#    else:
-#        transferAccsAllFGs = np.zeros((numFGs, len(testDataTags)))
-#
-#    resultFDName = "result_"+labelName+"_"+sourceDataTag
-#    if not os.path.exists(resultFDName):
-#            os.makedirs(resultFDName)
-#
-#    accs = np.zeros((numFGs, 2))
-#    optConfigs = {}
-#    for nfIdx in range(numFGs):
-#        start_time = time.time()
-#        numFeat = numFeats[nfIdx]
-#        print("\n[Using {} features]".format(numFeat))
-#
-#        if testDataTags != None:
-#            testDataBank = {}
-#            for dataTag in testDataTags:
-#                dataDir=dataDirPrefix+"_"+labelName+"_"+dataTag
-#                dataFP = os.path.join(dataDir, str(numFeat)+"feats.csv")
-#                testDataBank[dataTag] = pd.read_csv(dataFP)
-#            
-#            sourceData = testDataBank[sourceDataTag]
-#        else:
-#            sourceDataDir=dataDirPrefix+"_"+labelName+"_"+sourceDataTag
-#            sourceDataFP = os.path.join(sourceDataDir, str(numFeat)+"feats.csv")
-#            sourceData = pd.read_csv(sourceDataFP)
-#
-#        Y = sourceData['class'].to_numpy()
-#        X = sourceData.loc[:, sourceData.columns != "class"].to_numpy()
-#
-#        train_index, test_index = next(StratifiedShuffleSplit(test_size=test_ratio, random_state=123).split(X,Y))
-#        X_train, X_test = X[train_index], X[test_index]
-#        y_train, y_test = Y[train_index], Y[test_index]
-#
-#        opt = GPyOpt.methods.BayesianOptimization(
-#                f = partial(fitCV, X_train=X_train, y_train=y_train, cv=cv, domain=domain, clf_name=clf_name),  # function to optimize
-#                domain = domain,         # box-constrains of the problem
-#                acquisition_type ='EI')       # EI, MPI, LCB acquisition
-#        opt.run_optimization(max_iter=itersBO)
-#        #opt.plot_convergence()
-#
-#        x_best = opt.x_opt
-#        best_params = mapConfig(x_best, domain, clf_name=clf_name)
-#        print("[Best Config]: {}".format(best_params))
-#        optModel = createModel(best_params, clf_name)
-#        optModel.fit(X_train, y_train)
-#
-#        trainAcc = round(optModel.score(X_train, y_train), 4)
-#        testAcc = round(optModel.score(X_test, y_test), 4)
-#        print("[{}] Train score: {}, Test score: {}. ({} seconds)".format(
-#            numFeat, trainAcc, testAcc, time.time()-start_time) )
-#        accs[nfIdx, 0] = trainAcc
-#        accs[nfIdx, 1] = testAcc
-#
-#        optConfigs["numFeat"+str(numFeat)] = {"modelParams": best_params, "testAcc": testAcc}
-#        bestModelFP = os.path.join(
-#                resultFDName,
-#                saveFileNamePrefix+"_Model.sav")
-#        pickle.dump(optModel, open(bestModelFP, "wb"))
-#
-#        # save test acc cross datasets
-#        if testDataTags:
-#            transferAccsPerFeatGroup = []
-#            for testDataTag in testDataTags:
-#                testData = testDataBank[testDataTag]
-#                teY = testData['class'].to_numpy()
-#                teX = testData.loc[:, sourceData.columns != "class"].to_numpy()
-#                transferAccsPerFeatGroup.append(round(optModel.score(teX, teY), 4))
-#
-#            transferAccsAllFGs[nfIdx] = np.array(transferAccsPerFeatGroup)            
-#
-#    # Save optimal configs
-#    json_str = json.dumps(optConfigs, indent=4)
-#    optConfigsFP = os.path.join(
-#            resultFDName,
-#            saveFileNamePrefix+"_OptConfigs.json")
-#    with open(optConfigsFP, "w") as fp:
-#        print(json_str, file=fp)
-#
-#    # Save Training and Testing Accuracies
-#    accsFP = os.path.join(
-#            resultFDName,
-#            saveFileNamePrefix+"Accs.csv")
-#    npColumnNames = ["TrainAcc", "TestAcc"]
-#    npRowName = "NumFeatures"
-#    npRowValues = numFeats 
-#    NP2CSV(accs, npColumnNames, npRowName, npRowValues, fp=accsFP)
-#    plotAcc(figID, numFeats, accs, clf_name, test_ratio, resultFDName, itersBO)
-#
-#    return transferAccsAllFGs
-#
 
