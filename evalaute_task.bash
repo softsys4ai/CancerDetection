611bb0d3fe1aa22bd5c3d181760eeea387167ee3
Jianhai Su <suj@email.sc.edu>
2020-04-25 19:43:47 -0400

    a bash script for combining feature extraction and model training

---
+++ evalaute_task.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# python <script_name> <classifier_name> <label_name> <class_weight> <original_data_filepath>
+#    classifier_name  = "SVM", "RF"
+#    label_name =  "Diagnosis", "Class"
+#    class_weight = "equal", "balanced", float
+#    original_data_filepath: Optional. It is only for "Diagnosis" Task
+#       e.g., 
+
+TASK=$1 #"AgeGroup"
+echo -e "Task: detect ${TASK}"
+
+python Bo.py SVM ${TASK} equal  
+python Bo.py SVM ${TASK} balanced  
+
+
+python Bo.py RF ${TASK} equal  
+python Bo.py RF ${TASK} balanced  
+
